708fa77b5f6026cde0506596ff63b06f00c2bdfa
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-12-16 21:54:33 +0100

    Update SAM evaluation and remove installation files from git (#8)
    * Update SAM evaluation and remove installation files from git
    
    * Add measurement widget WIP
    
    * Add SAM eval results
    
    * Update measurement logic

--- run_segmentation_interactive.py
+++ run_segmentation_interactive.py
@@ -1,31 +1,39 @@
 import argparse
 import os
+from glob import glob
 from typing import List
 
 import h5py
 import imageio.v3 as imageio
+import napari
 import numpy as np
+import pandas as pd
+
 from micro_sam.sam_annotator import image_series_annotator
 from micro_sam.instance_segmentation import get_amg, get_predictor_and_decoder
+from napari.utils.notifications import show_info
+from qtpy.QtWidgets import QDockWidget, QPushButton
 from tqdm import tqdm
 
 from oct_tools.postprocessing import postprocess_segmentation
 from oct_tools.precompute_segmentation import _derive_prompts_sam, _segment_from_prompts
 from oct_tools.segmentation_utils import run_measurement
+from oct_tools.layer_information import identify_layers
 
 
 def _precompute_segmentation(images, sam_model_path, output_folder, postprocess=True):
     """Precompute segmentation using SAM.
     """
-
     predictor, decoder = get_predictor_and_decoder(model_type="vit_b", checkpoint_path=sam_model_path)
 
     # Create the segmenter.
     segmenter = get_amg(predictor, is_tiled=False, decoder=decoder)
+    os.makedirs(output_folder, exist_ok=True)
 
     for i, image in tqdm(enumerate(images), desc="Precompute segmentation", total=len(images)):
         output_path = os.path.join(output_folder, f"seg_{i:05}.tif")
-        table_out = os.path.join(output_folder, f"meas_{i:05}.tsv")
+        if os.path.exists(output_path):
+            continue
 
         # Init the segmenter for this image.
         segmenter.initialize(image, verbose=False)
@@ -37,10 +45,18 @@ def _precompute_segmentation(images, sam_model_path, output_folder, postprocess=
         if postprocess:
             seg = postprocess_segmentation(seg, image)
 
-        tab = run_measurement(seg)
-        tab.to_csv(table_out, sep="\t", index=False)
-
         imageio.imwrite(output_path, seg)
+
+
+def _find_call_button(viewer, button_text):
+    for dw in viewer.window._qt_window.findChildren(QDockWidget):
+        root = dw.widget()
+        if root is None:
+            continue
+        for b in root.findChildren(QPushButton):
+            if b.text() == button_text:
+                return b
+    raise RuntimeError(f"Could not find a QPushButton with text={button_text!r}")
 
 
 def run_annotator(
@@ -71,11 +87,29 @@ def run_annotator(
 
     if precompute_segmentation:
         _precompute_segmentation(images, sam_model, output_folder, postprocess=postprocess)
-    # TODO add Napari widget for saving measurements after annotation
-    # use run_measurement(seg) and obtain the viewer from the function
-    image_series_annotator(
-        images, output_folder, model_type="vit_b", checkpoint_path=sam_model, skip_segmented=False
+
+    # TODO use option for loading precomputed segmentations from another folder once this is in micro-sam master.
+    viewer = image_series_annotator(
+        images, output_folder, model_type="vit_b", checkpoint_path=sam_model,
+        skip_segmented=False, return_viewer=True,
     )
+
+    def post_measurement():
+        segmentation = viewer.layers["committed_objects"].data
+        n_layers = len(np.unique(segmentation)) - 1
+        layer_mapping = identify_layers(segmentation, expected_number_of_layers=n_layers)
+        layer_mapping = pd.DataFrame(dict(label_id=layer_mapping.keys(), layer=layer_mapping.values()))
+        measurements = run_measurement(segmentation, extra_columns=layer_mapping)
+        i = len(glob(os.path.join(output_folder, "measurement*.tsv")))
+        table_out = os.path.join(output_folder, f"measurement_{i:05}.tsv")
+        measurements.to_csv(table_out, sep="\t", index=False)
+        show_info(f"Measurements for {n_layers} layers saved to {table_out}")
+
+    # Get the next image button and bind the measurement function to it.
+    next_image_button = _find_call_button(viewer, "Next Image [N]")
+    next_image_button.clicked.connect(post_measurement)
+
+    napari.run()
 
 
 def main():
@@ -85,7 +119,7 @@ def main():
     parser.add_argument("-i", "--input", required=True, help="Input image.")
     parser.add_argument("-o", "--output", required=True, help="Output folder.")
     parser.add_argument("-z", "--slices", nargs="+", type=int, required=True, help="Slice(s) in z-direction.")
-    parser.add_argument("--model", default="./oct-sam-v3.pt", help="The SAM model trained for OCT data model.")
+    parser.add_argument("--model", default="./oct-sam-v4.pt", help="The SAM model trained for OCT data model.")
     parser.add_argument("--precompute_segmentation", action="store_true",
                         help="Pre-compute segmentation using prompts derived from SAM prediction.")
 
